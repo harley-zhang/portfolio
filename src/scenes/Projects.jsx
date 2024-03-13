90a7daaaf8ba608a8cda76f8421cffd7cd028159
harley-zhang <harleyzgolego@gmail.com>
2024-03-13 13:07:09 -0400

    updates to projects page

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -2,84 +2,84 @@ import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
 
 const container = {
-    hidden: {},
-    visible: {
-        transition: { staggerChildren: 0.2 }
-    }
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
 };
 
 const projectVariant = {
-    hiden: { opacity: 0, scale: 0.8 },
-    visible: { opacity: 1, scale: 1 },
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
 };
 
 const Project = ({ title }) => {
-    const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-white z-30 flex flex-col justify-center items-center text-center p-16 text-dark-blue`;
-    const projectTitle = title.split(" ").join("-").toLowerCase();
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-80 transition duration-500
+    bg-white z-30 flex flex-col justify-center items-center text-center p-16 text-dark-blue font-semibold`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
 
-    return (
-        <motion.div variants={projectVariant} className="relative">
-            <div className={overlayStyles}>
-                <p className="text-2xl font-helvetica">{title}</p>
-                <p className="mt-7">
-                    asdkjasldkjasldkjasldkajsdlkasjdlaskdjalskdszlj
-                </p>
-            </div>
-            <img src={`../assets/${projectTitle}.png`} />
-        </motion.div>
-    )
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-helvetica">{title}</p>
+        <p className="mt-7">
+          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+          porttitor accumsan tincidunt.
+        </p>
+      </div>
+      <img src={`../assets/${projectTitle}.png`} alt={projectTitle} />
+    </motion.div>
+  );
 };
 
 const Projects = () => {
-    return (
-        <section id="projects" className="pt-48 ph-48">
-            {/* HEADINGS */}
-            <motion.div
-                className="md:w-2/4 mx-auto text-center"
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true, amount: 0.5 }}
-                transition={{ duration: 0.5 }}
-                variants={{
-                    hidden: { opacity: 0, y: -50 },
-                    visible: { opacity: 1, y: 0 },
-                }}
-            >
-                <div>
-                    <p className="font-helvetica font-semibold text-4xl">
-                        <span className="text-dark-blue">PRO</span>JECTS
-                    </p>
-                    <div>
-                        <LineGradient width="w-1/3" />
-                    </div>
-                </div>
-                <p className="mt-10 mb-10 font-playfair">
-                    alskdjasldkjasdlkasjdlaskdjaslkdjasldkjasldkasjdolj
-                </p>
-                <Project title="Project 1" />
-                <Project title="Project 2" />
+  return (
+    <section id="projects" className="pt-48 pb-48">
+      {/* HEADINGS */}
+      <motion.div
+        className="md:w-2/5 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
+        <div>
+          <p className="font-helvetica font-semibold text-4xl">
+            Projects
+          </p>
+          <div className="flex justify-center mt-5">
+            <LineGradient width="w-2/3" />
+          </div>
+        </div>
+        <p className="mt-10 mb-10 font-playfair">
+          Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
+          fames odio in at. At magna ornare dictum lectus. Purus massa morbi
+          purus nec eget eleifend ut elit.
+        </p>
+      </motion.div>
 
-                {/* ROW 2 */}
-                <Project title="Project 1" />
-                <Project title="Project 2" />
-            </motion.div>
-
-            {/* PROJECTS */}
-            <div className="flex justify-center">
-                <motion.div
-                    className="sm:grid sm:grid-cols-3"
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    variants={container}
-                >
-                    <div className="flex justify-center text-center items-center p-10 bg-dark-blue max-w-[400px] max-h-[400px] text-2xl font-helvetica font-semibold">
-                        adslkdjalk
-                    </div>
-                </motion.div>
-            </div>
-        </section>
-    )
-}
+      {/* PROJECTS */}
+      <div className="flex justify-center">
+        <motion.div
+          className="sm:grid sm:grid-cols-3"
+          variants={container}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+        >
+          <Project title="EcoForecast" />
+          <Project title="Personal Portfolio" />
+          <Project title="Project 3" />
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
 export default Projects;
