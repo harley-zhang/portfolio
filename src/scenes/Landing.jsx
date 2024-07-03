114bd59ddb5f2d9ef3cc7267f5f7533f08967b22
harley-zhang <harleyzgolego@gmail.com>
2024-07-02 21:46:29 -0400

    added new animations, blurring effects, colours to navbar and homepage

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,8 +1,5 @@
-import SocialMediaIcons from "../components/SocialMediaIcons";
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
-import AnchorLink from "react-anchor-link-smooth-scroll";
-import profile from "../assets/profile-image.png";
 
 const Landing = ({ setSelectedPage }) => {
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
@@ -12,16 +9,6 @@ const Landing = ({ setSelectedPage }) => {
             id="home" 
             className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
         >
-
-            {/* IMAGES */}
-            <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
-                <img
-                    alt="profile"
-                    className="z-10 w-full max-w-[400px] md:max-w-[600px]"
-                    src={profile}
-                />
-            </div>
-
             {/* LANDING TEXT */}
             <div className={`z-30 basis-2/5 mt-12 md:mt-32 mx-[5%]`}>
                 {/* HEADINGS */}
@@ -42,41 +29,6 @@ const Landing = ({ setSelectedPage }) => {
                     <p className="mt-10 font-playfair mb-7 text-md text-center md:text-start">
                         Interested in web development, data science, computer engineering, and environmental sustainability.
                     </p>
-                </motion.div>
-
-                {/* BUTTONS */}
-                <motion.div
-                    className="flex mt-5 justify-center md:justify-start"
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    transition={{ delay: 0.2, duration: 0.4 }}
-                    variants={{
-                        hidden: { opacity: 0, x: -50 },
-                        visible: { opacity: 1, x: 0 }
-                    }}
-                >
-                    <AnchorLink
-                        className="bg-navy-blue text-light-blue rounded-sm py-3 px-7 font-semibold hover:opacity-80 transition duration-500"
-                        onClick={() => setSelectedPage("contact")}
-                        href="#contact"
-                    >
-                        Contact Me
-                    </AnchorLink>
-                </motion.div>
-
-                <motion.div
-                    className="flex mt-5 justify-center md:justify-start"
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    transition={{ delay: 0.4, duration: 0.4 }}
-                    variants={{
-                        hidden: { opacity: 0, x: -50 },
-                        visible: { opacity: 1, x: 0 }
-                    }}
-                >
-                    <SocialMediaIcons />
                 </motion.div>
             </div>
         </section>
--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,42 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 
-const Link = ({ page, selectedPage, setSelectedPage }) => {
+const Link = ({ page, selectedPage, setSelectedPage, onClick }) => {
     const lowerCasePage = page.toLowerCase();
     return (
-        <AnchorLink 
-            className={`inline-block ${selectedPage === lowerCasePage ? "underline" : ""}
-                hover:underline`}
+        <AnchorLink
+            className={`inline-block`}
             href={`#${lowerCasePage}`}
-            onClick={() => setSelectedPage(lowerCasePage)}
+            onClick={() => {
+                setSelectedPage(lowerCasePage);
+                onClick(); // Call the onClick handler to toggle off the menu
+            }}
         >
             {page}
         </AnchorLink>
     );
 };
 
-
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
-    const navbarBackground = isTopOfPage ? "" : "bg-black text-white transition duration-200";
+    const [navbarBackground, setNavbarBackground] = useState("");
+
+    useEffect(() => {
+        if (!isAboveSmallScreens && isMenuToggled) {
+            document.body.style.overflow = "hidden";
+            setNavbarBackground(""); // Clear navbar background when menu is toggled on small screens
+        } else {
+            document.body.style.overflow = "visible";
+            setNavbarBackground(isTopOfPage ? "" : "bg-[#0a0a0a] text-white transition duration-200 bg-opacity-70 backdrop-filter backdrop-blur-lg");
+        }
+    }, [isMenuToggled, isAboveSmallScreens, isTopOfPage]);
+
+    const handleToggleMenu = () => {
+        setIsMenuToggled(!isMenuToggled);
+    };
 
     return (
-        <nav className = {`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
-            <div className = "flex items-center justify-between mx-auto w-5/6">
-                <h4 className = "font-helvetica text-3xl font-semibold">hz●</h4>
+        <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-3`}>
+            <div className="flex items-center justify-between mx-auto w-5/6">
+                <h4 className="font-helvetica text-[15px]">hz</h4>
 
                 {/* DESKTOP NAV*/}
                 {isAboveSmallScreens ? (
-                    <div className = "flex justify-between gap-16 font-helvetica text-md font-medium">
+                    <div className="flex justify-between gap-14 font-helvetica text-[13px] font-medium">
                         <Link
-                            page="Home"
+                            page="Projects"
                             selectedPage={selectedPage}
                             setSelectedPage={setSelectedPage}
                         />
                         <Link
-                            page="Projects"
+                            page="Education"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Experience"
                             selectedPage={selectedPage}
                             setSelectedPage={setSelectedPage}
                         />
@@ -47,42 +67,53 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
                         />
                     </div>
                 ) : (
-                    <button 
-                        className = "flex justify-between gap-16 font-helvetica text-sm font-medium" 
-                        onClick={() => setIsMenuToggled(!isMenuToggled)}
-                    >Menu</button>
-            )}
+                    <button
+                        className="flex justify-between gap-16 font-helvetica text-sm font-medium"
+                        onClick={handleToggleMenu}
+                    >
+                        Menu
+                    </button>
+                )}
 
-            {/* MOBIlE MENU POPUP */}
-            {!isAboveSmallScreens && isMenuToggled && (
-                <div className = "fixed right-0 bottom-0 h-full bg-black text-white w-[300px]">
-                    {/*CLOSE ICON */}
-                    <div className="flex justify-end p-12 font-helvetica text-sm font-medium">
-                        <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                            Close
-                        </button>
-                    </div>
+                {/* MOBILE MENU POPUP */}
+                {!isAboveSmallScreens && isMenuToggled && (
+                    <div className="fixed right-0 bottom-0 h-full w-full bg-gradient-to-t from-transparent to-[#0a0a0a] bg-opacity-50 backdrop-filter backdrop-blur-xl text-white">
+                        {/* CLOSE ICON */}
+                        <div className="flex justify-end p-12 font-helvetica text-sm font-medium">
+                            <button onClick={handleToggleMenu}>
+                                Close
+                            </button>
+                        </div>
 
-                    {/* MENU ITEMS */}
-                    <div className = "flex flex-col gap-10 ml-[33%] text-sm">
-                        <Link
-                            page="Home"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                        />
-                        <Link
-                            page="Projects"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                        />
-                        <Link
-                            page="Contact"
-                            selectedPage={selectedPage}
-                            setSelectedPage={setSelectedPage}
-                        />
+                        {/* MENU ITEMS */}
+                        <div className="flex flex-col gap-[35px] ml-[20px] text-[25px]">
+                            <Link
+                                page="Projects"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                                onClick={handleToggleMenu}
+                            />
+                            <Link
+                                page="Education"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                                onClick={handleToggleMenu}
+                            />
+                            <Link
+                                page="Experience"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                                onClick={handleToggleMenu}
+                            />
+                            <Link
+                                page="Contact"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                                onClick={handleToggleMenu}
+                            />
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
             </div>
         </nav>
     );
