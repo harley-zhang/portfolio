101c82e6d13844de7efa4da215cf6eeb20c807e5
harley-zhang <harleyzgolego@gmail.com>
2024-07-04 13:45:07 -0400

    added projects.jsx

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -1,9 +1,13 @@
-//import EcoForecast from "../assets/ecoforecast.png";
-//import SimpleWeather from "../assets/simpleweather.png";
-//import portfolio from "../assets/portfolio.png";
+import EcoForecast from "../assets/ecoforecast.png";
+import SimpleWeather from "../assets/simpleweather.png";
+import portfolio from "../assets/portfolio.png";
 
 const Projects = () => {
-
+    return (
+        <section id="projects">
+            
+        </section>
+    )
 };
 
 export default Projects;
