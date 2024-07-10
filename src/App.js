52c39644c29dd7e55e5e283a35c1ef69aa1d12d5
harley-zhang <harleyzgolego@gmail.com>
2024-07-09 20:22:40 -0400

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Navbar from "./scenes/Navbar";
 import Landing from "./scenes/Landing";
 import Projects from "./scenes/Projects";
@@ -8,16 +8,7 @@ import Footer from "./scenes/Footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
-  const [isTopOfPage, setIsTopOfPage] = useState(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY === 0) setIsTopOfPage(true);
-      if (window.scrollY !== 0) setIsTopOfPage(false);
-    }
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  const [isTopOfPage] = useState(true);
 
   return (
     <div className="app bg-grey">
