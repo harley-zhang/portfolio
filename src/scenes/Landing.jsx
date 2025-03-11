a9522fc11fb9dbc1b0fef957809b4e4f7a86213d
Harley Zhang <harleyzgolego@gmail.com>
2025-03-10 20:31:17 -0400

    fixes

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -48,90 +48,59 @@ const Landing = () => {
 
             {/* MAIN CONTAINER */}
             <div className="flex flex-col gap-10 mt-10">
-                {/* Flex container for the two columns */}
-                <div className="flex flex-col md:flex-row gap-10">
-                    {/* TLDR Section - Left Column */}
-                    <div className="w-full md:w-1/2 flex justify-start md:justify-start">
-                        <div className="mt-7 text-[0.9rem] xs:text-[1.1rem] text-start text-grey-present">
-                            <p>TL;DR</p>
-                            {/* Stats */}
-                            <div className="flex flex-col">
-                                <div className="flex items-center justify-start text-[1.2rem]">
-                                    <div className="flex items-center justify-center h-12 bg-grey rounded-lg text-white">
-                                        Software Engineering @<span className="ml-1 font-sf-medium">University of Waterloo</span>
-                                    </div>
-                                </div>
-                                <div className="flex items-center justify-start text-[1.3rem]">
-                                    <div className="flex items-center justify-center h-12 rounded-lg text-white">
-                                        <span className="font-mono text-md px-2 py-1 mr-3 rounded-md bg-grey-present-bg text-grey-present">{sweCount}</span> SWE internships
-                                    </div>
-                                </div>
-                                <div className="flex items-center justify-start text-[1.3rem]">
-                                    <div className="flex items-center justify-center h-12 bg-grey rounded-lg text-white">
-                                        <span className="font-mono text-md px-2 py-1 mr-3 rounded-md bg-grey-present-bg text-grey-present">{hackathonsCount}</span> hackathons
-                                    </div>
-                                </div>
-                                <div className="flex items-center justify-start text-[1.3rem]">
-                                    <div className="flex items-center justify-center h-12 bg-grey rounded-lg text-white">
-                                        <span className="font-mono text-md px-2 py-1 mr-3 rounded-md bg-grey-present-bg text-grey-present">{githubCount}</span> public projects
-                                    </div>
-                                </div>
+
+
+                {/* Stack Section - Right Column */}
+                <div className="w-full md:w-1/2 flex md:justify-center">
+                    <div className="mt-7 text-[0.9rem] xs:text-[1.1rem] text-start text-grey-present">
+                        <p>Stack</p>
+                        <div className="flex items-center justify-start mt-2">
+                            {/* Box with fixed size and typewriter effect */}
+                            <div className="font-mono text-[0.9rem] bg-grey-present-bg p-4 rounded-md w-[calc(100vw-2.5rem)] md:w-[450px] h-[180px] overflow-hidden">
+                                <Typewriter
+                                    options={{
+                                        strings: [
+                                            `<span class="text-[#6b9955]">// LANGUAGES</span><br />` +
+                                            `<span class="text-[#569bd5]">Python</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#b5cea8]">JavaScript/Typescript</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#cb8e09]">C++</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#4ec9b0]">R</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#c485bf]">Bash</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#dcdcaa]">MySQL</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#9cdcfe]">SQLite</span><br />` +
+                                            `<br />` +
+                                            `<span class="text-[#6b9955]">// TECHNOLOGIES</span><br />` +
+                                            `<span class="text-[#4ec9b0]">Git</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#9cdcfe]">Google Cloud</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#c485bf]">React</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#cb8e09]">Node.js</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#b5cea8]">Linux</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#dcdcaa]">Unix</span><span class="text-[#8b8b8b]">, </span>` +
+                                            `<span class="text-[#569bd5]">HTML/CSS</span>`
+                                        ],
+                                        autoStart: true,
+                                        loop: true,
+                                        delay: 10,
+                                        deleteSpeed: 5,
+                                        pauseFor: 200000000
+                                    }}
+                                />
                             </div>
                         </div>
                     </div>
-
-                    {/* Stack Section - Right Column */}
-                    <div className="w-full md:w-1/2 flex md:justify-center">
-                        <div className="mt-7 text-[0.9rem] xs:text-[1.1rem] text-start text-grey-present">
-                            <p>Stack</p>
-                            <div className="flex items-center justify-start mt-2">
-                                {/* Box with fixed size and typewriter effect */}
-                                <div className="font-mono text-[0.9rem] bg-grey-present-bg p-4 rounded-md w-[calc(100vw-2.5rem)] md:w-[450px] h-[180px] overflow-hidden">
-                                    <Typewriter
-                                        options={{
-                                            strings: [
-                                                `<span class="text-[#6b9955]">// LANGUAGES</span><br />` +
-                                                `<span class="text-[#569bd5]">Python</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#b5cea8]">JavaScript/Typescript</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#cb8e09]">C++</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#4ec9b0]">R</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#c485bf]">Bash</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#dcdcaa]">MySQL</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#9cdcfe]">SQLite</span><br />` +
-                                                `<br />` +
-                                                `<span class="text-[#6b9955]">// TECHNOLOGIES</span><br />` +
-                                                `<span class="text-[#4ec9b0]">Git</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#9cdcfe]">Google Cloud</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#c485bf]">React</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#cb8e09]">Node.js</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#b5cea8]">Linux</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#dcdcaa]">Unix</span><span class="text-[#8b8b8b]">, </span>` +
-                                                `<span class="text-[#569bd5]">HTML/CSS</span>`
-                                            ],
-                                            autoStart: true,
-                                            loop: true,
-                                            delay: 10,
-                                            deleteSpeed: 5,
-                                            pauseFor: 200000000
-                                        }}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
 
-                {/* BUTTON */}
-                <div className="flex mt-10 items-center">
-                    <a href="mailto:harley.zhang@uwaterloo.ca" className="rounded-r-sm">
-                        <div className="text-[.8rem] xs:text-[.91rem] bg-white text-grey-dark transition duration-500 w-full h-full flex items-center justify-center px-6 py-2 rounded-3xl hover:opacity-80 relative">
-                            harley.zhang@uwaterloo.ca <GoArrowUpRight size={17} className="ml-1" />
-                        </div>
-                    </a>
-                </div>
             </div>
-        </section>
+
+            {/* BUTTON */}
+            <div className="flex mt-10 items-center">
+                <a href="mailto:harley.zhang@uwaterloo.ca" className="rounded-r-sm">
+                    <div className="text-[.8rem] xs:text-[.91rem] bg-white text-grey-dark transition duration-500 w-full h-full flex items-center justify-center px-6 py-2 rounded-3xl hover:opacity-80 relative">
+                        harley.zhang@uwaterloo.ca <GoArrowUpRight size={17} className="ml-1" />
+                    </div>
+                </a>
+            </div>
+        </section >
     );
 };
 
