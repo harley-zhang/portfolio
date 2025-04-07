17b310063e41eccf73a82c1d7e8e1692977e330d
Harley Zhang <78547210+harley-zhang@users.noreply.github.com>
2025-04-07 16:32:02 -0400

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <section className="about-section">
       <h1 className="logo-text">Harley Zhang</h1>
-      <p className="intro-text">Most of my work involves software engineering, machine learning, and data analysis. If you have any ideas or questions, please reach out.</p>
+      <p className="intro-text">Most of my work involves software engineering, machine learning, and data science. If you have any ideas or questions, please reach out.</p>
       
       <div className="action-container">
         <a href="mailto:harley.zhang@uwaterloo.ca" className="email-button">
