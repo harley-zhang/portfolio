f8808bb14236b0f85bfdec467955f5998cf76a69
Harley Zhang <harleyzgolego@gmail.com>
2025-08-28 22:43:27 -0400

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { Analytics } from '@vercel/analytics/react';
 
 function Intro() {
   return (
@@ -162,6 +163,7 @@ function App() {
           <Route path="/sagebrush.pdf" element={<Pdf />} />
         </Routes>
       </Router>
+      <Analytics />
     </div>
   );
 }
