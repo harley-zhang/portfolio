f80fd67aa35c2a63343d6ff2f183a7694c4ea378
harley-zhang <harleyzgolego@gmail.com>
2026-02-04 13:42:17 -0500

    fix

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_8feea188._.css",
-  "static/chunks/node_modules_@vercel_analytics_dist_react_index_mjs_b73c06df._.js",
-  "static/chunks/node_modules_next_dist_d64dadd3._.js",
-  "sta
